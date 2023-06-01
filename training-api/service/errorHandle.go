87dc055c36994f79d4e5c9f769226be81415e615
ntaka-0303 <n-takamatunaoki0310@gmail.com>
2023-06-01 14:03:59 +0900

    backend:logging

--- training-api/service/errorHandle.go
+++ training-api/service/errorHandle.go
@@ -1,11 +1,18 @@
 package service
 
 import (
+	"io"
 	"log"
 	"net/http"
+	"os"
 )
 
+const log_file_path = "./var/log/training-api.log"
+
 func ErrorHandle(w http.ResponseWriter, err error) {
-	log.Printf("Error: %v", err)
+	logfile, _ := os.OpenFile(log_file_path, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0666)
+	log.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
+	log.SetOutput(io.MultiWriter(logfile, os.Stderr))
+	log.Panicf("Error: %v", err)
 	http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 }
